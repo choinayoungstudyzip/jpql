5857dc06d183816a641a3de3a7dda1d44641df74
SSG\tic0vw <tic0vw@shinsegae.com>
2025-12-05 11:16:09 +0900

    JPQL 타입 표현과 기타식

---
+++ src/main/java/jpql/JpaMain5.java
@@ -0,0 +1,57 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain5 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush(); // DB에 반영
+            em.clear(); // 영속성 컨텍스트 비움
+
+            // enum은 setParameter 안하고 하드코딩할거면 패키지명까지 적어줘야함
+            String query =  "select m.username, 'HELLO', TRUE from Member m where m.type = :userType";
+            List<Object[]> result = em.createQuery(query)
+                    .setParameter("userType", MemberType.ADMIN)
+                    .getResultList();
+
+            for (Object[] objects : result) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
